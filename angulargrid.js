a77f7b7bb3406a3caaa2ec023c8aa2a4e5bf7883
Sudhanshu Yadav <sudhanshuyadav2@gmail.com>
2015-11-18 20:16:00 +0530

    manual refresh issue fixed

--- angulargrid.js
+++ angulargrid.js
@@ -1,5 +1,5 @@
 /*
-    angularGrid.js v 0.3.4
+    angularGrid.js v 0.3.5
     Author: Sudhanshu Yadav
     Copyright (c) 2015 Sudhanshu Yadav - ignitersworld.com , released under the MIT license.
     Demo on: http://ignitersworld.com/lab/angulargrid/demo1.html
